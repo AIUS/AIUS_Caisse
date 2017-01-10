6db73ff462f7dbf4e9b256f5f21bfe74ba942029
Luka Vandervelden <lukc@upyum.com>
2017-01-10 10:46:20 +0100

    Primitive DB connections middleware.

--- src/AIUS_Caisse.cr
+++ src/AIUS_Caisse.cr
@@ -1,7 +1,30 @@
 require "kemal"
 
-get "/" do
-  "Vous êtes tous chou !"
+require "db"
+require "pg"
+
+class HTTP::Server::Context
+	def db=(@db : DB::Database)
+	end
+	def db
+		@db
+	end
+end
+
+class CustomHandler < Kemal::Handler
+	def call(context)
+		DB.open "postgres:///aius_caisse" do |db|
+			context.db = db
+
+			call_next context
+		end
+	end
+end
+
+add_handler CustomHandler.new
+
+get "/" do |context|
+	context.db.to_s
 end
 
 Kemal.run
