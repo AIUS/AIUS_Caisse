ef346447385967f08e553e8f8745c43a7ebae893
caswitch <mariefrance.kom@gmail.com>
2017-01-10 17:17:50 +0100

    Ajout de la route PUT /picture/:id

--- src/AIUS_Caisse.cr
+++ src/AIUS_Caisse.cr
@@ -28,6 +28,16 @@ get "/" do |context|
 end
 
 class Product
+	def name=(@name) end
+	def category=(@category) end
+	def price=(price)
+		if price < 0
+			raise "price cant be negative"
+		end
+
+		@price = price
+	end
+
 	DB.mapping({
 		id: Int32,
 		name: String,
@@ -62,15 +72,25 @@ class Category
 	})
 end
 
-get "/categories" do |context|
-	Category.from_rs(context.db.query "SELECT id, name FROM product_category").to_json
-end
-
 def error(message)
 	{
 		"status" => "error",
 		"message" => message,
 	}.to_json
+end
+
+get "/categories" do |context|
+	Category.from_rs(context.db.query "SELECT id, name FROM product_category").to_json
+end
+
+get "/product/:id" do |context|
+	id = context.params.url["id"]
+	product = context.db.query_one "SELECT id, name, category, price FROM product WHERE id = $1", id, as: Product
+	if product.nil?
+		error "product not found"
+	else
+		product.to_json	
+	end
 end
 
 post "/products" do |context|
@@ -95,6 +115,31 @@ end
 delete "/product/:id" do |context|
 	id = context.params.url["id"]
 	context.db.exec "DELETE FROM product WHERE id = $1", id
+end
+
+put "/product/:id" do |context|
+	id = context.params.url["id"]
+	product = context.db.query_one "SELECT id, name, category, price FROM product WHERE id = $1", id, as: Product
+	if product.nil?
+		error "product not found"
+	else
+		name = context.params.json["name"]?.as?(String)
+		if !name.nil?
+			product.name = name
+		end
+		category = context.params.json["category"]?.as?(Int32)
+		if !category.nil?
+			product.category = category
+		end
+		p = context.params.json["price"]?
+		price = p.as?(Int64) || p.as?(Float64)
+		if !price.nil?
+			product.price = price.to_f64
+		end
+
+		context.db.exec "UPDATE product SET (name, category, price) = ($2, $3, $4) WHERE id = $1", product.id, product.name, product.category, product.price
+		product.to_json
+	end
 end
 
 Kemal.run
