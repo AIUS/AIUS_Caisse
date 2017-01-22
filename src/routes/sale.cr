29902d83ac1a074d2a594c7a636f5467f0e54701
Mickael BAUER <mickael.bauer7@gmail.com>
2017-01-22 14:41:11 +0100

    Update Sale and post /sale.

--- src/routes/sale.cr
+++ src/routes/sale.cr
@@ -15,7 +15,7 @@ get "/sale/" do |context|
 			dend = Time.parse(dend,"%Y-%m-%d")
 		end
 	rescue
-		next error "Syntax Date incorect"
+		next error "Incorrect syntax Date."
 	end
 
 	sales = Sale.from_rs(context.db.query "SELECT id,seller,date FROM sale WHERE $1::date < date and date <= $2::date ",dbegin,dend)
@@ -26,59 +26,20 @@ get "/sale/" do |context|
 end 
 
 post "/sale/" do |context|
-	sale_products = context.params.json["data"]?.as?(Array(JSON::Type))
+	saleProducts = context.params.json["data"]?.as?(Array(JSON::Type))
 	if context.user.nil?
-		error "unauthorized user"
-	elsif sale_products.nil?
-		error "Incorrect JSON"	
+		error "Unauthorized user."
+	elsif saleProducts.nil?
+		error "Incorrect Array of products."	
 	else
-		ids = [] of Int64
-		stack = nil
-		i = 0
-		while i < sale_products.size
-			sale_product = sale_products[i].as?(Hash(String,JSON::Type))
-			i += 1
-			if sale_product.nil?
-				stack = error "Incorrect JSON 2"
-				break
-			else
-				id = sale_product["id"]?.as?(Int64)
-				qt = sale_product["quantity"]?.as?(Int64)	
-				if id.nil? || qt.nil?
-					stack = error "Incorrect JSON 3"
-					break
-				elsif qt < 1
-					stack = error "Incorrect quantity"
-					break
-				elsif ids.includes?(id)
-					stack = error "Incorrect product"
-					break
-				elsif (context.db.query_one? "SELECT id FROM product WHERE id = $1",id, as: Int32).nil?
-					stack = error "Product not found"
-					break
-				else
-					ids.insert(-1,id)
-				end
-			end
+		begin
+			sale = Sale.new("110e8400-e29b-11d4-a716-446655440000")#FIXME uuid de l'user
+			sale<< saleProducts
+			sale.save(context.db)
+		rescue er
+			next error er.message
 		end
-		if !stack.nil?
-			next stack
-		end
-		sale = context.db.scalar "INSERT INTO sale (seller) VALUES ('110e8400-e29b-11d4-a716-446655440000') RETURNING id"
-		i = 0
-		while i < sale_products.size
-			sale_product = sale_products[i].as?(Hash(String,JSON::Type))
-			i += 1
-			if !sale_product.nil?
-				id = sale_product["id"]?.as?(Int64)
-				qt = sale_product["quantity"]?.as?(Int64)
-				if !id.nil? && !qt.nil?
-					context.db.exec "INSERT INTO sale_product (sale,product,quantity) VALUES ($1,$2,$3)" , sale,id,qt
-				end
-			end
-		end
-
-	 	(context.db.query_one? "SELECT id,seller,date FROM sale WHERE id = $1",sale,as: Sale).to_json
+		{	"status" => "OK"}.to_json
 	end
 end
 
@@ -91,5 +52,4 @@ delete "/sale/:id" do |context|
 		error "No row affected."
 	end
 end
-
 
--- src/sale.cr
+++ src/sale.cr
@@ -1,27 +1,52 @@
-
 require "db"
 
 class Sale
-	@sale_products = [] of SaleProduct
+	@id = 0
+	@seller = ""
+	@saleProducts = [] of SaleProduct
+	@date = Time.now
+	def initialize(s)
+		self.seller=s
+	end
 	def	seller=(@seller) end#FIXME test uuid
-	
+	def id=(@id)end
+	def id 
+		@id 
+	end
+
 	def <<(pd)
-		if @sale_products.any? { |sale_product| sale_product.product = pd.product}
-				raise "Already id of product"
+		if @saleProducts.any? { |saleProduct| saleProduct.product = pd.product}
+			raise "Already id of product."
 		end
-			@sale_products << pd
+			@saleProducts << pd
 	end
 
-	def <<(pd : Array(SaleProduct))
-		i = 0
-		while i < pd.size
-			self<< pd[i]
-			i =+ 1
-		end
+	def <<(pds : Array(SaleProduct))
+		pds.each {|pd| self<< pd}
 	end
 
+	def <<(pds : Array(JSON::Type))
+		pds.each { |pd|	self<< SaleProduct.new(pd)}
+	end
+	
 	def searchProduct(db)
 		self<< (SaleProduct.from_rs(db.query "SELECT sale, product, quantity FROM sale_product WHERE sale = $1",@id))
+	end
+
+	def save(db)
+		@saleProducts.each do |pd| 
+			if pd.isNotInDB(db)
+				raise "Product not found."
+			end
+		end
+		id = (db.scalar "INSERT INTO sale (seller) VALUES ($1) RETURNING id", @seller).as?(Int32)
+		if !id.nil?
+			@id=id
+			@saleProducts.each do |pd|
+				pd.sale= @id
+				pd.save(db)
+			end
+		end
 	end
 
 	DB.mapping({
@@ -33,19 +58,48 @@ class Sale
 	JSON.mapping({
 		id: Int32,
 		seller: String,
-		sale_products: Array(SaleProduct),
+		saleProducts: Array(SaleProduct),
 		date: Time
 	})
 end
 
 class SaleProduct
+	@sale = 0
+	@product = 0
+	@quantity = 0
+	def initialize(pd : JSON::Type)
+		pd = pd.as?(Hash(String,JSON::Type))
+		if pd.nil?
+			raise "Not a JSON valid."
+		else
+			id = pd.["id"]?.as?(Int64)
+			qt = pd.["quantity"]?.as?(Int64)
+			if id.nil?
+				raise "No id of product."
+			elsif qt.nil?
+				raise "No quantity of product."
+			else
+				self.product=id.to_i
+				self.quantity=qt.to_i
+			end
+		end
+	end
+
 	def sale=(@sale) end
-	def product=(@sale) end
+	def product=(@product) end
 	def quantity=(qt)
 		if qt > 0
-			raise "Quantity can't be negative"
+			raise "Quantity can't be negative."
 		end
 		@quantity=qt
+	end
+
+	def isNotInDB(db)
+		(db.query_one? "SELECT id FROM product WHERE id = $1", @product, as: Int32).nil?
+	end
+
+	def save(db)
+		db.exec "INSERT INTO sale_product (sale, product, quantity) VALUES ($1,$2,$3)", @sale,@product,@quantity
 	end
 
 	DB.mapping({
