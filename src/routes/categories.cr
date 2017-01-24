6b3b8550fc17df555da7635a9944281c88abd260
caswitch <mariefrance.kom@gmail.com>
2017-01-24 22:14:08 +0100

    Récupération des résultats des exec pour les produits et les catégories.

--- src/routes/categories.cr
+++ src/routes/categories.cr
@@ -8,28 +8,39 @@ require "../error"
 require "../categories"
 
 get "/categories" do |context|
-	context.response.content_type = "application/json"
-	Category.from_rs(context.db.query "SELECT id, name FROM product_category").to_json
+	if context.user.nil?
+		error "unauthorized user"
+	else
+		Category.from_rs(context.db.query "SELECT id, name FROM product_category").to_json
+	end
 end
 
 get "/category/:id" do |context|
-	id = context.params.url["id"]
-	category =  context.db.query_one? "SELECT id, name FROM product_category WHERE id = $1", id, as: Category
-	if category.nil?
-		error "category not found"
+	if context.user.nil?
+		error "unauthorized user"
 	else
-		category.to_json	
+		id = context.params.url["id"]
+		category =  context.db.query_one? "SELECT id, name FROM product_category WHERE id = $1", id, as: Category
+		if category.nil?
+			error "category not found"
+		else
+			category.to_json	
+		end
 	end
 end
 
 post "/categories" do |context|
-	name = context.params.json["name"]?.as?(String)
-
-	if name.nil?
-		error "missing `name` field"
+	if context.user.nil?
+		error "unauthorized user"
 	else
-		new_id = context.db.scalar "INSERT INTO product_category (name) VALUES ($1) RETURNING id", name
-		(context.db.query_one? "SELECT id, name FROM product_category WHERE id = $1", new_id, as: Category).to_json
+		name = context.params.json["name"]?.as?(String)
+
+		if name.nil?
+			error "missing `name` field"
+		else
+			new_id = context.db.scalar "INSERT INTO product_category (name) VALUES ($1) RETURNING id", name
+			(context.db.query_one? "SELECT id, name FROM product_category WHERE id = $1", new_id, as: Category).to_json
+		end
 	end
 end
 
@@ -47,14 +58,15 @@ delete "/category/:id" do |context|
 				"status" => "ok"
 			}.to_json
 		end
-
 	end
 end
 
 put "/category/:id" do |context|
 	id = context.params.url["id"]
 	category = context.db.query_one? "SELECT id, name FROM product_category WHERE id = $1", id, as: Category
-	if category.nil?
+	if context.user.nil?
+		error "unauthorized user"
+	elsif category.nil?
 		error "category not found"
 	else
 		name = context.params.json["name"]?.as?(String)
@@ -62,7 +74,12 @@ put "/category/:id" do |context|
 			category.name = name
 		end
 
-		context.db.exec "UPDATE product_category SET (name) = ($2) WHERE id = $1", category.id, category.name
-		category.to_json
+		result = context.db.exec "UPDATE product_category SET (name) = ($2) WHERE id = $1", category.id, category.name
+
+		if result.rows_affected == 0
+			error "category not found"
+		else
+			category.to_json
+		end
 	end
 end
